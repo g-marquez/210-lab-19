84bb03973ba3bee524d40ff9c9224308fc6905cf
Gabe Marquez <gabrielmarquez98@gmail.com>
2026-03-08 18:12:22 -0700

    Initial commit: starting lab 19

---
+++ 210-lab-19.cpp
@@ -0,0 +1,10 @@
+//*****************************************************************************
+// COMSC-210 | Lab 19 | Gabriel Marquez
+// Description: this program abstracts lab 18 by one layer, automates it such
+// that review comments are read from an external file, and the movie rating is
+// a random double between 1.0 and 5.0..
+//*****************************************************************************
+
+#include <iostream>
+#include <string>
+using namespace std;
